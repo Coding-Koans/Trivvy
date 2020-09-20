791ed6c59f0c35fab85ce314ff4a0494de2920b4
Ian Carroll <ian.d.carroll@gmail.com>
2020-09-20 12:58:21 -0700

    Abstract away FileSystem from players so it can be used by questions_asked too

--- src/game/records/players.py
+++ src/game/records/players.py
@@ -1,18 +1,20 @@
-import os
+from src.game.records.file_system_orm import FS_ORM
 
 class Players():
     def __init__(self, filename):
-        self.filename = f"{filename}.txt"
-        self.cache = None
-        self.scores = self.get_scores_from_FS()
+        blank_records = {}
+        self.fs = FS_ORM(filename, blank_records)
+        self.scores = self.fs.get_records()
 
     def score(self, player):
-        self.get_scores()
+        self.fs.get_records()
+        print("HEY! OVER HERE!")
+        print(self.scores)
         if player in self.scores.keys():
             self.up_score(player)
         else:
             self.add_to_board(player)
-        self.save_scores()
+        self.fs.save_records()
 
     def up_score(self, player):
         self.scores[player]['round_points'] += 1
@@ -26,23 +28,23 @@ class Players():
         }
 
     def score_winners(self, winners):
-        self.get_scores()
+        self.fs.get_records()
         for player in winners:
             # pretty sure it's impossible for someone to win and not be on the board
             if player in self.scores.keys(): 
                 self.scores[player]["game_wins"] += 1
-        self.save_scores()
+        self.fs.save_records()
 
     def reset_scores_for_next_round(self):
-        self.get_scores()
+        self.fs.get_records()
         self.reset("round_points")
-        self.save_scores()
+        self.fs.save_records()
 
     def reset_scores_for_next_game(self):
-        self.get_scores()
+        self.fs.get_records()
         self.reset("round_points")
         self.reset("game_points")
-        self.save_scores()
+        self.fs.save_records()
 
     def reset(self, thing_to_be_reset):
         for score in self.scores.values():
@@ -64,38 +66,3 @@ class Players():
                 player_tuples.append((player, record[point_type]))
         sorted_players = sorted(player_tuples, key=lambda player: player[1], reverse=True)
         return sorted_players[:3]
-
-    def get_scores(self):
-        self.scores = self.get_scores_from_FS()
-
-    def save_scores(self):
-        self.overwrite_scores_to_FS()
-
-    def get_scores_from_FS(self):
-        blank_scores = {}
-        if self.cache or self.cache == blank_scores:
-            return self.cache
-
-        if os.path.exists(self.filename):
-            return self.read_from_existing_record()
-
-        self.scores = blank_scores
-        self.save_scores()
-        self.cache = blank_scores
-        return blank_scores
-
-    def read_from_existing_record(self):
-        file = open(self.filename)
-        scores = eval(file.read())
-        self.cache = scores
-        file.close()
-        return scores
-
-    def overwrite_scores_to_FS(self):
-        file = open(self.filename, "wt")
-        file.write(str(self.scores))
-        self.clear_score_cache()
-        file.close()
-
-    def clear_score_cache(self):
-        self.cache = None
