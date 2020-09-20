c3db3262e46e4c0e331a2a556004363e75155a39
Ian Carroll <ian.d.carroll@gmail.com>
2020-09-20 16:29:34 -0700

    Questions)_Asked uses the same fs_orm that players uses

--- src/game/records/questions_asked.py
+++ src/game/records/questions_asked.py
@@ -1,15 +1,21 @@
+from src.game.records.file_system_orm import FS_ORM
+
 class Questions_Asked():
     
-    def __init__(self):
-        self._log = []
-        self._clear_received = False
+    def __init__(self, filename = "questions_asked"):
+        new_log = []
+        self.fs = FS_ORM(filename, new_log)
+        self.log = self.fs.get_records()
 
     def log(self, question):
-        self._log.append(question)
+        self.log = self.fs.get_records()
+        self.log.append(question)
+        self.fs.save_records(self.log)
 
     def clear_all(self):
-        self._log = []
-        self._clear_received = True
+        self.log = []
+        self.fs.save_records(self.log)
 
     def all_logged(self):
-        return self._log
+        self.log = self.fs.get_records()
+        return self.log
