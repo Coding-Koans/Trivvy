58c5e7b8e09147f9302fb538ee458fd89dc75e35
Ian Carroll <ian.d.carroll@gmail.com>
2020-09-20 18:59:55 -0700

    Test that questions_asked actually works

--- src/game/records/questions_asked.py
+++ src/game/records/questions_asked.py
@@ -5,17 +5,17 @@ class Questions_Asked():
     def __init__(self, filename = "questions_asked"):
         new_log = []
         self.fs = FS_ORM(filename, new_log)
-        self.log = self.fs.get_records()
+        self._log = self.fs.get_records()
 
     def log(self, question):
-        self.log = self.fs.get_records()
-        self.log.append(question)
-        self.fs.save_records(self.log)
+        self._log = self.fs.get_records()
+        self._log.append(question)
+        self.fs.save_records(self._log)
 
     def clear_all(self):
-        self.log = []
-        self.fs.save_records(self.log)
+        self._log = []
+        self.fs.save_records(self._log)
 
     def all_logged(self):
-        self.log = self.fs.get_records()
-        return self.log
+        self._log = self.fs.get_records()
+        return self._log
--- test/game/records/test_questions_asked.py
+++ test/game/records/test_questions_asked.py
@@ -1,20 +1,48 @@
 import unittest
-import os
+from test.test_helpers.file_system import cleanup
 from src.game.records.questions_asked import Questions_Asked as Subject
 
 class Questions_Asked_TestCase(unittest.TestCase):
-    def cleanup(self, filename):
-        if os.path.exists(f"{filename}.txt"):
-            os.remove(f"{filename}.txt")
+    file_name = "mock_questions_asked"
+    question = {
+        'Ask': "What's a Diorama?",
+        'Answer': "OMG Han! Chewie! They're all here!"
+    }
 
-    def skip_test_questions_asked_log_creates_a_log_with_a_single_entry_upon_creation(self):
-        pass
+    def test_questions_asked_log_creates_a_log_with_a_single_entry_upon_creation(self):
+        given = Subject(self.file_name)
+        actual = given.all_logged()
+        cleanup(self.file_name)
+        self.assertEqual(actual, [])
 
-    def skip_test_questions_asked_log_updates_the_log_with_a_new_entry(self):
-        pass
+    def test_questions_asked_log_updates_the_log_with_a_new_entry(self):
+        given = Subject(self.file_name)
 
-    def skip_test_questions_asked_clear_game_replaces_the_log_with_an_empty_list(self):
-        pass
+        given.log(self.question)
 
-    def skip_test_questions_asked_log_returns_the_current_logged_questions(self):
-        pass
+        actual = given.all_logged()
+        cleanup(self.file_name)
+        self.assertEqual(actual, [ self.question ])
+
+    def test_questions_asked_log_updates_the_log_with_a_ditional_entries(self):
+        given = Subject(self.file_name)
+
+        given.log(self.question)
+        given.log(self.question)
+        given.log(self.question)
+
+        actual = given.all_logged()
+        cleanup(self.file_name)
+        self.assertEqual(actual, [ self.question, self.question, self.question ])
+
+    def test_questions_asked_clear_game_replaces_the_log_with_an_empty_list(self):
+        given = Subject(self.file_name)
+        given.log(self.question)
+        given.log(self.question)
+        given.log(self.question)
+
+        given.clear_all()
+
+        actual = given.all_logged()
+        cleanup(self.file_name)
+        self.assertEqual(actual, [])
