cafb674fd88e9d741d30afe2e0234bc3ec1bc704
Ian Carroll <ian.d.carroll@gmail.com>
2020-09-28 19:47:59 -0700

    Use modern commands in the mock socket for simpler dry-runs

--- mocks/socket.py
+++ mocks/socket.py
@@ -38,12 +38,13 @@ class fake_reception:
             channel_message = f':{chan}!{chan}@{chan}.tmi.twitch.tv PRIVMSG #{chan} :This is the Trivvy Channel Speaking: Be excellent to each other.\r\n'
             user_message = f':happy_lass!happy_lass@happy_lass.tmi.twitch.tv PRIVMSG #{chan} :Woot!'
             user_answer = f':trivvy_lad!trivvy_lad@trivvy_lad.tmi.twitch.tv PRIVMSG #{chan} :The Great Answer 42'
-            user_command = f':trivvy_fan!trivvy_fan@trivvy_fan.tmi.twitch.tv PRIVMSG #{chan} :!score'
-            admin_command = f':{admin}!{admin}@{admin}.tmi.twitch.tv PRIVMSG #{chan} :!loadconfig'
+            user_command = f':trivvy_fan!trivvy_fan@trivvy_fan.tmi.twitch.tv PRIVMSG #{chan} :!go'
+            admin_command = f':{admin}!{admin}@{admin}.tmi.twitch.tv PRIVMSG #{chan} :!go'
+            admin_stop_command = f':{admin}!{admin}@{admin}.tmi.twitch.tv PRIVMSG #{chan} :!stop'
             self_message = f':{bot}!{bot}@{bot}.tmi.twitch.tv PRIVMSG #{chan} :You shouldn\'t be seeing this message'
             ping_message = 'PING :tmi.twitch.tv\r\n'
             empty_message = ''
 
-            messages = [channel_message, user_message, user_answer, user_command, admin_command, self_message, ping_message, empty_message]
+            messages = [channel_message, user_message, user_answer, user_command, admin_command, admin_stop_command, self_message, ping_message, empty_message]
 
             return random.choice(messages)
