7b0238d7f8a2b3fc72dd5506c86cb85f649a29a1
Ian Carroll <ian.d.carroll@gmail.com>
2020-09-28 18:55:18 -0700

    Timer correctly converts poll intervals to seconds

--- src/game/timer.py
+++ src/game/timer.py
@@ -1,34 +1,36 @@
+import time
+
 class Timer:
-
-    # Needs to know about the connection's seconds_per_message
-    #    so that it can guage it's timing against that rythm
-
-    # Needs to know about the configuration
-    #    so that it can calibrate itself based on the admin's config
-
-    # it be called from Go, knows about hat it cares about
-
-    # example things it cares about:
-        # {
-        #     self.trivia_hinttime_1,
-        #     self.trivia_hinttime_2,
-        #     self.trivia_skiptime,
-        #     self.trivia_questiondelay,
-        # }
 
     def __init__(self, connection_poll_tempo, config_settings):
         self.settings = config_settings
         self.tempo = connection_poll_tempo
+        self._times_asked = 0
 
     def start_question_timer(self):
         self._times_asked = 0
 
     def question_time_up(self):
         self._times_asked += 1
-        return self._times_asked >= 18
+        return self._times_asked >= self.q_max_asks()
 
     def question_hint_1_up(self):
-        return self._times_asked >= 4
+        return self._times_asked >= self.h1_max_asks()
 
     def question_hint_2_up(self):
-        return self._times_asked >= 8
+        return self._times_asked >= self.h2_max_asks()
+
+    def wait(self):
+        time.sleep(self.settings['wait'])
+
+    def q_max_asks(self):
+        return self.max_for('times_up')
+
+    def h1_max_asks(self):
+        return self.max_for('hint_1_up')
+
+    def h2_max_asks(self):
+        return self.max_for('hint_2_up')
+
+    def max_for(self, setting):
+        return self.settings[setting] / self.tempo
